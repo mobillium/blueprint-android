bf02de45aec566a1cd770f4b59ae8df345c964de
Omer Karaca <omerkaracca@gmail.com>
2022-06-08 14:43:57 +0300

    Small fixes

---
+++ sample/src/main/kotlin/com/mobillium/blueprint/sample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.mobillium.blueprint.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author karacca
+ * @date 8.06.2022
+ */
+ 
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
