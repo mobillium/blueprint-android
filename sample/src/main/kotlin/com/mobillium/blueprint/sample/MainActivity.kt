bfaea32cf146143952e518a367759199272170d9
Omer Karaca <omerkaracca@gmail.com>
2022-06-08 14:46:21 +0300

    Fix lint issues

--- sample/src/main/kotlin/com/mobillium/blueprint/sample/MainActivity.kt
+++ sample/src/main/kotlin/com/mobillium/blueprint/sample/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
  * @author karacca
  * @date 8.06.2022
  */
- 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
